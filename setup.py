f838445a6ec9b236e4d96c080f485a74d32b3934
fauzaanu <fauzaangasim@gmail.com>
2023-10-23 14:34:51 +0500

    copy requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt',encoding="utf-8") as f:
+# Specify the full path to the requirements.txt file
+requirements_file = 'requirements.txt'
+
+with open(requirements_file, encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
     name='agf',
     version='0.1',
-    description='Your package description',
-    author='Your Name',
-    author_email='your.email@example.com',
+    description='Article Generation Framework',
+    author='Fauzaan Gasim',
+    author_email='hello@fauzaanu.com',
     packages=find_packages(),
     install_requires=requirements,
 )
