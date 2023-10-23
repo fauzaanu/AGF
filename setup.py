7d3f4740001295e584848acfbc620ae559a88057
fauzaanu <fauzaangasim@gmail.com>
2023-10-23 14:23:01 +0500

    BOM.....

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open('requirements.txt',encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
