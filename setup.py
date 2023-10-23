83466cc6415e766209756dd62c23372b23635f56
fauzaanu <fauzaangasim@gmail.com>
2023-10-23 14:11:10 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='agf',
+    version='0.1',
+    description='Your package description',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=requirements,
+)
