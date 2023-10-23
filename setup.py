fa39fe36856d05be4baaa0da98d28df3c9cfbdeb
fauzaanu <fauzaangasim@gmail.com>
2023-10-23 14:52:48 +0500

    long description eh

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
 
+long_description = '''
+# Article Generation Framework
+
+This is a framework for generating articles using OpenAI's GPT-3 API.
+'''
+
+
+
+
+
+
+
+
 setup(
     name='agf',
     version='0.1',
     description='Article Generation Framework',
+    long_description=long_description,
     author='Fauzaan Gasim',
     author_email='hello@fauzaanu.com',
     packages=find_packages(),
