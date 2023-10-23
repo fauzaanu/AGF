64bdcbcf49898ff055b9bbb68ba522062fc65f55
fauzaanu <fauzaangasim@gmail.com>
2023-10-23 14:42:55 +0500

    requirements.txt errors

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Specify the full path to the requirements.txt file
-requirements_file = 'requirements.txt'
+requirements_file = './requirements.txt'
 
 with open(requirements_file, encoding="utf-8") as f:
     requirements = f.read().splitlines()
